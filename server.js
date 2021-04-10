26366810fac1eff31b6a49b2b4b6fa59062cb2ea
Tekseon Shin <tekseon.shin@gmail.com>
2021-04-10 21:29:59 +0900

    update settings

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+// var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = ["http://sarang.org","https://sarang.org","http://www.sarang.org","https://www.sarang.org"];
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -18,7 +19,8 @@ function parseEnvList(env) {
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 // var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
-var checkRateLimit = require('./lib/rate-limit')("100 1");
+// 1분에 10000회.  sarang.org 및 서브도메인들은 무제한
+var checkRateLimit = require('./lib/rate-limit')("10000 1 /(.*\.)?sarang\.org/");
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
@@ -43,10 +45,14 @@ cors_proxy.createServer({
     // 'x-forwarded-port',
   ],
   redirectSameOrigin: true,
+  // If set, an Access-Control-Max-Age request header with this value (in seconds) will be added.
+  corsMaxAge: 60*60*24*30,
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
+  // If set, a https.Server will be created
+  // httpsOptions: true,
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
